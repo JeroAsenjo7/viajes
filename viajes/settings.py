54ef6e5add5f4c8ccfa59d5daca487aa6c8c58ba
JeroAsenjo7 <jeroasenjo@gmail.com>
2026-04-21 10:58:57 -0300

    gmail

--- viajes/settings.py
+++ viajes/settings.py
@@ -89,10 +89,11 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'pao.valija.magica1@gmail.com')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'ncuy oqbp kxwr vjsb')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 EMAIL_DESTINO = os.environ.get('EMAIL_DESTINO', 'pao.valija.magica1@gmail.com')
+EMAIL_TIMEOUT = 30
 
 CSRF_TRUSTED_ORIGINS = [
     'https://*.railway.app'
