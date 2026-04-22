7e44c8262743d7c4218a10f9539418196331261d
JeroAsenjo7 <jeroasenjo@gmail.com>
2026-04-21 23:08:31 -0300

    fix email port

--- viajes/settings.py
+++ viajes/settings.py
@@ -87,8 +87,9 @@ LOGIN_URL = 'panel_login'
 # Email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
